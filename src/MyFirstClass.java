781c92ee367eeaa7b8f4b57e767c9e7b61dfe918
Vishal0802 <vishal.confi@gmail.com>
2021-08-31 02:04:25 +0530

    add inlocal repo

---
+++ src/MyFirstClass.java
@@ -0,0 +1,14 @@
+
+public class MyFirstClass {
+
+	public MyFirstClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Demo");
+
+	}
+
+}
